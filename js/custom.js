8bb81d8421c6430da7677eb0e37e786227fc519b
Md. Shahiduzzaman <Himel4534@users.noreply.github.com>
2021-10-02 16:09:59 +0600

    complete last two section

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function (){
     // });
     $(".testimonial_slide").owlCarousel({
         items: 1,
-        navText: ["<i class='fas fa-long-arrow-alt-left me-4 prev_arrow'></i>","<i class='fas fa-long-arrow-alt-right next_arrow'></i>"],
+        navText: ["<i class='fas fa-long-arrow-alt-left prev_arrow'></i>","<i class='fas fa-long-arrow-alt-right next_arrow'></i>"],
         nav: true,
         loop: true,
         smartSpeed: 700,
