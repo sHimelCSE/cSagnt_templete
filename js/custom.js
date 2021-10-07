d4838c9a0f4d17a52b5c212582d415f31041553d
Md. Shahiduzzaman <Himel4534@users.noreply.github.com>
2021-10-08 00:39:21 +0600

    Modify one
    fixed some error and solve some responsive problem and video pop up add.

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,21 @@
-$(document).ready(function (){
-    // $(".testimonial_slide").slick({
-    //     autoplay: true,
-    //     prevArrow: "<i class='fas fa-long-arrow-alt-left me-4 prev_arrow'></i>",
-    //     nextArrow: "<i class='fas fa-long-arrow-alt-right next_arrow'></i>",
-    // });
+$(document).ready(function () {
     $(".testimonial_slide").owlCarousel({
         items: 1,
-        navText: ["<i class='fas fa-long-arrow-alt-left prev_arrow'></i>","<i class='fas fa-long-arrow-alt-right next_arrow'></i>"],
+        navText: ["<i class='fas fa-long-arrow-alt-left prev_arrow'></i>", "<i class='fas fa-long-arrow-alt-right next_arrow'></i>"],
         nav: true,
         loop: true,
         smartSpeed: 700,
-        
+    });
+    $(".venobox").venobox({
+        framewidth: "70%",
+        titleattr: "data-title",
+        titleBackground: '#ffffff',
+        titleColor: "#ab6db0",
+        titlePosition: 'top',
+        share: ["facebook", "linkedin", "pinterest", "twitter"],
+        spinner: "cube-grid",
+        spinColor: "#ab6db0",
     });
 });
-
-
-
-
-
-
-
-
-
-
-
 
 
